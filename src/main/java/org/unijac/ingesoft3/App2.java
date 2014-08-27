f3b88c6c78cc57be01810cbe80deb01caa48a7e6
Alex Roldan HD <aleshivan@gmail.com>
2014-08-27 00:57:12 -0500

    Interface proof

---
+++ src/main/java/org/unijac/ingesoft3/App2.java
@@ -0,0 +1,13 @@
+package org.unijac.ingesoft3;
+
+import org.unijac.ingesoft3.mvc.CalcView2;
+
+/**
+ * Created by alexhd on 8/26/14.
+ */
+public class App2 {
+    public static void main(String[] args) {
+        CalcView2 view = new CalcView2();
+        view.setVisible(true);
+    }
+}
