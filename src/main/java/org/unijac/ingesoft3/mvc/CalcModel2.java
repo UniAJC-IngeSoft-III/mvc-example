64598df5b6d46c6c408773199c392d712361e0bc
Alex Roldan HD <aleshivan@gmail.com>
2014-08-28 21:22:05 -0500

    fix type for numeric representation remove unused code fix scale and round mode

--- src/main/java/org/unijac/ingesoft3/mvc/CalcModel2.java
+++ src/main/java/org/unijac/ingesoft3/mvc/CalcModel2.java
@@ -1,54 +1,35 @@
 package org.unijac.ingesoft3.mvc;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.Stack;
+import java.math.RoundingMode;
 
 /**
  * Created by alexhd on 27/08/14.
  */
 public class CalcModel2 {
     private static final String INITIAL_VALUE = "0";
-    private BigInteger total;
-    private BigInteger last;
+    private BigDecimal total;
+    private BigDecimal last;
     private String operation;
-    private String error;
 
     public CalcModel2() {
         reset();
     }
 
     public void reset() {
-        total = new BigInteger(INITIAL_VALUE);
+        total = new BigDecimal(INITIAL_VALUE);
         operation = null;
         last = null;
-        error = null;
-    }
-
-    public void addTo(String value){
-        total = total.add(new BigInteger(value));
-    }
-
-    public void subTo(String value){
-        total = total.subtract(new BigInteger(value));
-    }
-
-    public void divideBy(String value){
-        total = total.divide(new BigInteger(value));
-    }
-
-    public void multiplyBy(){
-        total = total.multiply(last);
     }
 
     public void pushOperation(String operation){
         this.operation = operation;
         last = total;
-        total = new BigInteger(INITIAL_VALUE);
+        total = new BigDecimal(INITIAL_VALUE);
     }
 
     public void setValue(String value){
-        total = new BigInteger(value);
+        total = new BigDecimal(value);
     }
 
     public String getValue(){
@@ -57,25 +38,25 @@ public class CalcModel2 {
 
     public void doOperation() throws ArithmeticException{
         if(testOperation()){
-            BigInteger result;
+            BigDecimal result;
             switch (operation){
                 case "X":
-                    total = last.multiply(total);
+                    total = last.multiply(total).setScale(2, RoundingMode.HALF_UP);
                     last = null;
                     operation = null;
                     break;
                 case "/":
-                    total = last.divide(total);
+                    total = last.divide(total, 2, RoundingMode.HALF_UP);
                     last = null;
                     operation = null;
                     break;
                 case "+":
-                    total = last.add(total);
+                    total = last.add(total).setScale(2, RoundingMode.HALF_UP);
                     last = null;
                     operation = null;
                     break;
                 case "-":
-                    total = last.subtract(total);
+                    total = last.subtract(total).setScale(2, RoundingMode.HALF_UP);
                     last = null;
                     operation = null;
                     break;
